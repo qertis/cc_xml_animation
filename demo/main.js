23f007a8d184376ab1a7f7e10042b4bc55358358
Denis Baskovsky <baskovskiy@bifit.com>
2015-12-02 10:47:30 +0300

    demo created

---
+++ demo/main.js
@@ -0,0 +1,20 @@
+// ctor : function() {
+// this._super();
+
+var self = this;
+
+var animationSprite = new cc.Sprite();
+animationSprite.setPosition(cc.p(200, 200));
+animationSprite.setAnchorPoint(cc.p(0.5, 1));
+
+// put window.res animation_xml file
+var freakAnimation = new cc.Class.XMLAnimation(res['animation_xml']);
+freakAnimation.onLoaded = function () {
+  var frames = freakAnimation.getFrames();
+  var animation = new cc.Animation(frames, 0.05);
+
+  animationSprite.runAction(new cc.RepeatForever(new cc.Animate(animation)));
+  self.addChild(animationSprite, 1);
+};
+
+// }
